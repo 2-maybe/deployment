d2ef663116507b95be8103583d6b8b64ce882126
Abhiral Chhetri <abh20072027@gmail.com>
2025-10-22 22:17:35 +0530

    0.0.0.0

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/home" , (req ,res)=>{
     res.status(200).send(`<h1> Hello from home side </h1>`)
 })
 
-app.listen(4000 , ()=>{
+app.listen(4000 , '0.0.0.0' , ()=>{
     console.log("Listening on port 4000")
 })
